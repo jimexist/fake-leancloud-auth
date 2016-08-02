b8488d0cefe2cf0ded56795f16e339ef099b7eee
Jiayu Liu <jimexist@gmail.com>
2016-08-02 16:49:11 +0800

    adding phoenix

---
+++ lib/fake_leancloud_auth/repo.ex
@@ -0,0 +1,3 @@
+defmodule FakeLeancloudAuth.Repo do
+  use Ecto.Repo, otp_app: :fake_leancloud_auth
+end
---
+++ test/views/layout_view_test.exs
@@ -0,0 +1,3 @@
+defmodule FakeLeancloudAuth.LayoutViewTest do
+  use FakeLeancloudAuth.ConnCase, async: true
+end
