903e5eea9f8480b2cb38fe6e3891732a4e6041e5
Jiayu Liu <jimexist@gmail.com>
2016-08-02 23:26:30 +0800

    adding one simple trivial test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+const chai = require('chai')
+// const supertest = require('supertest')
+const expect = chai.expect
+
+describe('FakeLeancloudAuth', () => {
+  it('should work', () => {
+    expect(1).to.equal(1)
+  })
+})
